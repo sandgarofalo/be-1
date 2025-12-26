0949b06d3fbfdfe2c92cca8adbc76ccb05327c32
sandgarofalo <sand.garofalo@outlook.com>
2025-12-26 16:40:08 +0100

    add demo endpoints and code cleanup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from "express";
+import data from "../static/users.json";
 
 const app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get("/", (req, res) => {
-  res.send('<p style="background: red">hello world</p>');
+  res.send({ name: "testuser" });
+});
+
+app.get("/users", (req, res) => {
+  res.send(data);
 });
 
 app.listen(3000, () => {
